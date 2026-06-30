36f62b645cca385cc761eefbb3ca83f3d660d5f0
Neetoosan <tobimolla44@gmail.com>
2026-06-30 21:25:32 +0100

    fix: strip invalid XML control characters in escapeXml function

--- excel-builder.js
+++ excel-builder.js
@@ -306,6 +306,7 @@ function columnName(index) {
 
 function escapeXml(value) {
   return String(value)
+    .replace(/[\x00-\x08\x0B\x0C\x0E-\x1F]/g, "")
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
